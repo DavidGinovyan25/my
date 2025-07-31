d4698240d73ca8c6e7e2f01e8b106b3b96363f1a
DavidGinovyan25 <david48952@gmail.com>
2025-07-31 20:35:37 +0300

    build: add build

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/david/c++/prg1dag/src/error_handling.cpp" "CMakeFiles/main.dir/src/error_handling.cpp.o" "gcc" "CMakeFiles/main.dir/src/error_handling.cpp.o.d"
+  "/home/david/c++/prg1dag/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/david/c++/prg1dag/src/parse.cpp" "CMakeFiles/main.dir/src/parse.cpp.o" "gcc" "CMakeFiles/main.dir/src/parse.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
